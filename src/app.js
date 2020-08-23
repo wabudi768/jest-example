3b05cd6e8d5a58bfb7f7b2cdac060ff4a8a6358b
wabudi768 <wabudi768@gmail.com>
2020-08-23 20:19:31 +0700

    changes file

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.status(200).json({ name: 'john doe', age: 28 })
+  res.status(200).json({ name: 'john doe', age: 28 , city: 'jakarta', state: 'dki jakarta'})
 })
 
 module.exports = app
